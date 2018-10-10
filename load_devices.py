3d885d3e23c5fdd6e5d0681d1dee72f5c60f9816
srikrishna3118 <Sra1srinivas>
2018-10-10 18:33:01 +0530

    simple register logging

--- load_devices.py
+++ load_devices.py
@@ -3,6 +3,7 @@ import os, sys
 
 # logging
 import logging
+import time
 logger = logging.getLogger(__name__)
 # suppress debug messages from other modules used.
 logging.getLogger("requests").setLevel(logging.WARNING)
@@ -13,12 +14,35 @@ logging.getLogger("communication_interface").setLevel(logging.WARNING)
 #sys.path.insert(0, '../messaging')
 from ideam_messaging import *
 
-devices
-apps
+devices = ["device" + str(i) for i in range(100)]
+apps = ["application" + str(i) for i in range(1)]
+device_apikey=[]
 
-def register():
+def register_devices():
 
     # devices and apps:
-    devices = ["device" + str(i) for i in range(4)]
-    apps = ["application" + str(i) for i in range(1)]
+
+    i=0
+
+    for d in devices:
+        start_time = time.time()
+        success, apikey = register(d)
+        elapsed_time = time.time() - start_time
+        #print (success)
+        i=i+1
+        device_apikey.append(apikey)
+        print(time.strftime("%H:%M:%S", time.gmtime(elapsed_time)))
+
+def deregister_devices():
+    for d in devices:
+        start_time = time.time()
+        success = deregister(d)
+        elapsed_time = time.time() - start_time
+        print(time.strftime("%H:%M:%S", time.gmtime(elapsed_time)))
+        #print(success)
+
+if __name__=='__main__':
+    register_devices()
+    #print "-----------------------------------------------------"
+    deregister_devices()
 
